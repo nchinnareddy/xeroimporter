f62ab20913365bd69910d151ecf15bfc8da2bf44
Bui Thanh Hai <hai.bth@gmail.com>
2012-10-15 03:50:37 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xeroimporter::Application.config.secret_token = 'af86a5df9f5c3cbad40b7224f5f784387e769d8b8389b36a102a67cf405d4e1309227939bbf4d12f5af57c19a00fe152ad96d1125f774c5b5fe1146fdf00e097'
